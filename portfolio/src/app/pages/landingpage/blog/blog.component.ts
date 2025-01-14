f42c67a86371a1fa0c646ab8fb3a39711a39ef06
Torres-Neto <torresneto.tn@gmail.com>
2025-01-14 14:35:05 -0300

    título e email:To do Blog

--- portfolio/src/app/pages/landingpage/blog/blog.component.ts
+++ portfolio/src/app/pages/landingpage/blog/blog.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { VerMaisButtonComponent } from '@app/shared/components/ver-mais-button/ver-mais-button.component';
+import { BlogPost } from '@app/shared/interfaces/blogPost.interface';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { EmailInputComponent } from '../../../shared/components/email-input/email-input.component';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    InputIconModule,
+    IconFieldModule,
+    InputTextModule,
+    FloatLabelModule,
+    FormsModule,
+    ButtonModule,
+    RouterModule,
+    VerMaisButtonComponent,
+    EmailInputComponent,
+  ],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.scss'
+  styleUrl: './blog.component.scss',
 })
 export class BlogComponent {
+  emailValue: string = '';
 
+  blogPosts: BlogPost[] = [
+    {
+      image: 'assets/web-dev-2024.jpg',
+      title: 'As Tendências Mais Quentes em Desenvolvimento Web para 2024',
+      description:
+        'Descubra as tecnologias e práticas que estão moldando o futuro da web',
+      date: '13 Ago, 2025',
+      category: 'Landing Page',
+    },
+    {
+      image: 'assets/mobile-dev.jpg',
+      title:
+        'Desenvolvimento Mobile: Dicas Essenciais para Criar Apps de Sucesso',
+      description:
+        'Do design à implementação, tudo o que você precisa saber para desenvolver aplicativos móveis incríveis',
+      date: '08 Nov, 2025',
+      category: 'E-commerce',
+    },
+    {
+      image: 'assets/ai-ml.jpg',
+      title:
+        'Como a Inteligência Artificial Está Transformando o Desenvolvimento de Software',
+      description:
+        'Explorando o impacto da IA no desenvolvimento web e como você pode aproveitar essa tecnologia',
+      date: '14 Dez, 2025',
+      category: 'I.A.',
+    },
+  ];
 }
--- portfolio/src/app/pages/landingpage/projects/projects.component.ts
+++ portfolio/src/app/pages/landingpage/projects/projects.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CarouselProjectsComponent } from '@app/shared/components/carousel-projects/carousel-projects.component';
+import { VerMaisButtonComponent } from '@app/shared/components/ver-mais-button/ver-mais-button.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CarouselProjectsComponent],
+  imports: [CarouselProjectsComponent, VerMaisButtonComponent, ButtonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
