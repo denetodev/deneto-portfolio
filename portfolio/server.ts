9f5e3675309845a4ee35ac76cd420a651bcb9a7b
Torres-Neto <torresneto.tn@gmail.com>
2025-01-31 19:37:10 -0300

    Ajustes nos arquivos SCSS, migração de @import para @use e correção de variáveis

--- portfolio/server.ts
+++ portfolio/server.ts
@@ -1,9 +1,9 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { CommonEngine } from '@angular/ssr/node';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -20,10 +20,13 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  }));
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
@@ -37,8 +40,8 @@ export function app(): express.Express {
         publicPath: browserDistFolder,
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+      .then((html: string) => res.send(html))
+      .catch((err: Error) => next(err));
   });
 
   return server;
--- portfolio/src/app/pages/landingpage/landingpage.component.ts
+++ portfolio/src/app/pages/landingpage/landingpage.component.ts
@@ -2,14 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { HomeComponent } from './home/home.component';
-import { ProjectsComponent } from "./projects/projects.component";
-import { BlogComponent } from "./blog/blog.component";
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
 import { OfficesComponent } from './offices/offices.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [CommonModule, ButtonModule, HomeComponent, OfficesComponent, ProjectsComponent, BlogComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    HomeComponent,
+    OfficesComponent,
+    ProjectsComponent,
+    BlogComponent,
+  ],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss',
 })
