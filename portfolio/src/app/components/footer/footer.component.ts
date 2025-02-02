79a17877bad483f9111e172c7acf1e3c01ac7e34
Torres-Neto <torresneto.tn@gmail.com>
2025-02-02 00:10:52 -0300

    footer finalizado

--- portfolio/src/app/components/footer/footer.component.ts
+++ portfolio/src/app/components/footer/footer.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ContactButtonComponent } from '@app/shared/components/contact-button/contact-button.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ContactButtonComponent,
+    FormsModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    ButtonModule,
+    MenuModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
