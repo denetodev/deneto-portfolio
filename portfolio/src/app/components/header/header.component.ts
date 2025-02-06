20ce6e776d3262a8a82d90908a3d6880a6bd5cac
Torres-Neto <torresneto.tn@gmail.com>
2025-02-06 00:40:36 -0300

    ajuste do Header

--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { RippleModule } from 'primeng/ripple';
+import { Router } from '@angular/router';
+import { ScrollService } from '@app/shared/services/scroll.service';
 import { LanguageSwitcherComponent } from '../../shared/components/language-switcher/language-switcher.component';
 import { ContactButtonComponent } from '@app/shared/components/contact-button/contact-button.component';
-
-import { MenubarModule } from 'primeng/menubar';
-import { BadgeModule } from 'primeng/badge';
-import { AvatarModule } from 'primeng/avatar';
-import { InputTextModule } from 'primeng/inputtext';
-import { RippleModule } from 'primeng/ripple';
-import { MenuItem } from 'primeng/api';
-import { CommonModule } from '@angular/common';
-import { ScrollService } from '@app/shared/services/scroll.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -19,47 +16,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
   imports: [
     CommonModule,
+    ToolbarModule,
+    ButtonModule,
+    AvatarModule,
+    RippleModule,
     LanguageSwitcherComponent,
     ContactButtonComponent,
-
-    MenubarModule,
-    BadgeModule,
-    AvatarModule,
-    InputTextModule,
-    RippleModule,
   ],
 })
 export class HeaderComponent implements OnInit {
-  items: MenuItem[] | undefined;
+  isScrolled = false;
+  items = [
+    {
+      label: 'Sobre',
+      command: () => this.scrollToSection('sobre'),
+    },
+    {
+      label: 'Serviços',
+      command: () => this.scrollToSection('servicos'),
+    },
+    {
+      label: 'Projetos',
+      command: () => this.scrollToSection('projetos'),
+    },
+    {
+      label: 'Blog',
+      command: () => this.scrollToSection('blog'),
+    },
+  ];
 
   constructor(private router: Router, private scrollService: ScrollService) {}
 
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Sobre',
-        command: () => this.scrollToSection('sobre'),
-      },
-      {
-        label: 'Serviços',
-        command: () => this.scrollToSection('servicos'),
-      },
-      {
-        label: 'Projetos',
-        command: () => this.scrollToSection('projetos'),
-      },
-      {
-        label: 'Blog',
-        command: () => this.scrollToSection('blog'),
-      },
-    ];
+    this.onWindowScroll();
   }
 
   private scrollToSection(sectionId: string): void {
-    // Primeiro, garantimos que estamos na página inicial
     if (this.router.url !== '/') {
       this.router.navigate(['/']).then(() => {
-        // Após a navegação, esperamos um tick para garantir que o DOM foi atualizado
         setTimeout(() => {
           this.scrollService.scrollToElement(sectionId);
         }, 100);
@@ -71,5 +65,10 @@ export class HeaderComponent implements OnInit {
 
   navigateToLandingpage() {
     this.router.navigate(['/**']);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
   }
 }
